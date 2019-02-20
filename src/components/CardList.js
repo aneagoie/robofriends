b99dbd07bb00072372176f23660ad478b763bc71
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-02-19 20:41:06 -0800

    changed the width of cards

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
+import "./CardList.css";
 
 const CardList = ({ robots }) => {
   return (
     <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
     </div>
   );
-}
+};
 
 export default CardList;
