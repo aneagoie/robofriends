211625fe0c3c492947365fdcd7da0d13ab7d8e04
Shivam Tyagi <tshivam260@gmail.com>
2019-03-06 03:25:12 +0530

    making some changes to the project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+//import Card from './Card.js';  //const Card naam ka object present hai Card.js me //COMPONENT 1
+//import CardList from './CardList.js'; //COMPONENT 2
+import App from './App.js';
 import 'tachyons';
+import * as serviceWorker from './serviceWorker';
+//import {robots} from './robots.js';   //"const robots" naam ka object present hai Robots.js me
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render( 
+<App/>, document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
