db7a7acad1fe83ebda3af6f8ddad0bc97fe4c464
Bryan <windsor.bryan@gmail.com>
2019-03-15 14:13:23 -0400

    added errorboundary

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>OOOOOPS</h1>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
