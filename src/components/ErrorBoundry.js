b63b131a0582e0fb73d9e6eeb21f8e8aedc76914
michelleRahman21 <michgomez1807@hotmail.com>
2020-02-12 12:52:29 -0500

    Added ErrorBoundry in the project

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hasError: false
+    }
+  }
+componentDidCatch(error, info){
+  this.setState({hasError: true})
+}
+
+  render(){
+    if(this.state.hasError) {
+      return <h1>Ops! that is not good</h1>
+    }
+    return this.props.children
+  }
+}
+export default ErrorBoundry
