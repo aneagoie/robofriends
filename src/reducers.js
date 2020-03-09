91b841ec470a849f818907df2b48b628b6cf8293
michelleRahman21 <michgomez1807@hotmail.com>
2020-03-09 13:03:49 -0400

    Import the actions we have as an object
    Export the const for the 3 promises in one function using a switch
    
    Create another initial state for requestRobots
    
    Create another initial state for requestRobots

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,37 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js'
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from './constants';
 
-const initialState = {
-  setSearchField: ''
+const initialStateSearch = {
+  searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) => {
-  switch(action.type) {
+export const searchRobots = (state=initialStateSearch, action={} => {
+  switch (action.type) {
     case CHANGE_SEARCH_FIELD:
-     return Object.assign({}, state, {searchField: action.payload)};
-       default:
-         return state;
+     return Object.assign({}, state, {searchField: action.payload})
+    default:
+     return state
+  }
+})
+
+const initialStateRobots = {
+  robots: [],
+  isPending: true
+}
+
+export const requestRobots = (state=initialStateRobots, action={}) => {
+  switch (action.type) {
+     case REQUEST_ROBOTS_PENDING:
+       return Object.assign({}, state, {isPending: true})
+      case REQUEST_ROBOTS_SUCCESS:
+       return Object.assign({}, state, {robots: action.payload, isPending: false})
+      case REQUEST_ROBOTS_FAILED:
+       return Object.assign ({}, state, {error: action.payload})
+      default:
+        return state
   }
 }
