0b6f9fdd45dcfe836e165a03ba8f6d26a979046e
michelleRahman21 <michgomez1807@hotmail.com>
2020-03-09 12:46:58 -0400

    Import constants and export consts requestRobots function with dispatch, fetch, then, catch

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,16 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js'
+import { apiCall } from './api/api'
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_FAILED,
+  REQUEST_ROBOTS_SUCCESS
+} from './constants.js'
 
-export const setSearchField = (text) => ({
-  type: CHANGE_SEARCH_FIELD,
-  paylod: text
-})
+export const setSearchField = (text) -> ({ type: CHANGE_SEARCH_FIELD, payload: text})
+
+export const requestRobots = () = (dispatch) => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING})
+  apiCall('https://jsonplaceholder.typicode.com/users')
+    .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
+}
