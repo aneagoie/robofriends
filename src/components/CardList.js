6ef24bbbe9db84f6c28858070d44defc7c38fd76
Preet Shah <preet25@somaiya.edu>
2020-05-07 19:41:24 +0530

    added no search found concept

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,22 +2,26 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
-    </div>
-  );
+ return !robots.length ? 
+    <h1>Robot Not Found</h1> : 
+    ( <div > {
+            robots.map((user, i) => {
+                return <Card key = {
+                    i
+                }
+                id = {
+                    robots[i].id
+                }
+                name = {
+                    robots[i].name
+                }
+                email = {
+                    robots[i].email
+                }
+                />
+            })
+        } </div>
+    );
 }
 
 export default CardList;
