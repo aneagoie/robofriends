c753ccc4f6f67b3a04d1d8fc531b65d040e463b8
Artemijs Pavlovs <artemijs.pavlovs@artpav.io>
2021-02-04 21:18:15 +0100

    accesibility label text

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <label htmlFor="searchBox"></label>
+      <label htmlFor="searchBox">search box</label>
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
