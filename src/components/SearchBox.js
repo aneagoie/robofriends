ee4a528344c9e374bd229a61bce652d031d18b21
Artemijs Pavlovs <artemijs.pavlovs@artpav.io>
2021-02-04 21:17:50 +0100

    accesibility fix

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,10 +3,13 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
+      <label htmlFor="searchBox"></label>
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
+        id='searchBox'
         placeholder='search robots'
+        label
         onChange={searchChange}
       />
     </div>
