f44c249331fb463db738ecb7d4586c45e200969d
Artemijs Pavlovs <artemijs.pavlovs@artpav.io>
2021-02-05 22:29:50 +0100

    snapshot testing for card component

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -4,5 +4,5 @@ import { shallow } from "enzyme";
 import Card from "./Card";
 
 it('expect to render Card component', () => {
-  expect(shallow(<Card/>).length).toEqual(1)
+  expect(shallow(<Card/>)).toMatchSnapshot();
 })
