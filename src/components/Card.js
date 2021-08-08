34ff95bbf1e75245a3e97e69f5fe1b41c0c1e4fb
Nice1Rach <68408670+Nice1Rach@users.noreply.github.com>
2021-08-08 20:05:42 +1200

    Adding pages

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({ name, email, id }) => {
+  return (
+    <div className='tc grow bg-light-yellow br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/CardList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {
+        robots.map((user, i) => {
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+              />
+          );
+        })
+      }
+    </div>
+  );
+}
+
+export default CardList;
