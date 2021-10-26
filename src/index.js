26bb8b25be2115d208c231ecc8b3f91cf6f220f0
Andrei Neagoie <aneagoie@MacBook-Pro-2.lan>
2021-10-26 10:23:17 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
