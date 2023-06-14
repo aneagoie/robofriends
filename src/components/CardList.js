0d0b28cef202de773da3510d833b5bfb9ddef4b0
Willy-Friggin-V <williamventura711@gmail.com>
2023-06-14 12:21:45 -0400

    adding error change

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import Card from './Card';
+import Card from './Card'
 
-const CardList = ({ robots }) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
-    </div>
-  );
+const CardList = ({robots}) => {
+    // if(true) {
+    //     throw new Error ('NOOOOOOO')
+    // }
+    return(
+        <div>
+        {
+            robots.map((user, i) =>{
+                return (
+                    <Card
+                        key={i}
+                        id={robots[i].id}
+                        name={robots[i].name}
+                        email={robots[i].email}
+                    />
+                );
+            })
+        }
+        </div>
+    )
 }
 
 export default CardList;
