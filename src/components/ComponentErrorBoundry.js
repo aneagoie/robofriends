db8ebb74f0fc514326427e477d5092bccc7959dc
Lexiefinancial <Lexiefinancial@outlook.com>
2023-10-12 18:33:16 -0500

    First Commit

---
+++ src/components/ComponentErrorBoundry.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+    
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true})
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Oooooops. That is NOT good</h1>
+        }
+    return this.props.children
+    }   
+}
+
+export default ErrorBoundry;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ComponentErrorBoundry';
 import './App.css';
 
 
@@ -33,10 +34,12 @@ class App extends Component {
       <h1>Loading</h1> :
       (
         <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
+          <h1 className='f1'>RoboFam</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
